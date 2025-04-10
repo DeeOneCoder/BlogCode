03ca0fd809dbf642ab28600dc5ff0c8c358541ab
DeeOneCoder <101057409+DeeOneCoder@users.noreply.github.com>
2025-04-10 13:44:00 +0100

    Blog Project First Commit

---
+++ DavidoBlog/src/main/java/com/davido/DavidoBlog/DavidoBlogApplication.java
@@ -0,0 +1,13 @@
+package com.davido.DavidoBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavidoBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DavidoBlogApplication.class, args);
+	}
+
+}
---
+++ DavidoBlog/src/test/java/com/davido/DavidoBlog/DavidoBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davido.DavidoBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DavidoBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
